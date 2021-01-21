067a918615c720b7a77e29700daf4ce5957fda67
jamil-Eguiguren <73700004+jamil-Eguiguren@users.noreply.github.com>
2021-01-21 14:52:31 -0500

    Add files via upload

--- Ejercicios/src/ejercicios/Problema02.java
+++ Ejercicios/src/ejercicios/Problema02.java
@@ -16,6 +16,21 @@ public class Problema02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[][] datos = {{10, 2, 3}, {1, 5, 8}, {6, 2, 3}, {7, 8, 1}};
+        suma(datos, 0);
+    }
+
+    public static void suma(double[][] datos, int x) {
+        double suma = 0;
+        for (int columna = 0; columna < datos[columna].length; columna++) {
+            suma = suma + datos[x][columna];
+        }
+        System.out.println("La suma total es: " + suma);
+        x = x + 1;
+        if (x < datos.length) {
+            suma(datos, x);
+        }
     }
     
 }
+
